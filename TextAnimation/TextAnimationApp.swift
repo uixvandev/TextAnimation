b88f781231afe3645f10ba6bef608ad2b91419b2
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-08-17 00:00:03 +0700

    Initial Commit

---
+++ TextAnimation/TextAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextAnimationApp.swift
+//  TextAnimation
+//
+//  Created by irfan wahendra on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TextAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
