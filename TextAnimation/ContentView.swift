5af68e0b514442fac099fa8e6cb8d5b378f66522
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-08-17 00:40:09 +0700

    first

--- TextAnimation/ContentView.swift
+++ TextAnimation/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Yoffset: CGFloat = 0
+    @State var tapCount = 3
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Text("Level Up")
+            VStack(alignment: .leading, spacing: 20){
+                Text("20")
+                    .foregroundStyle(.green)
+                Text("21")
+                    .foregroundStyle(.orange)
+                Text("22")
+                    .foregroundStyle(.blue)
+            }
+            .offset(y:54 + Yoffset)
+            .mask({
+                Rectangle()
+                    .frame(height:50)
+            })
+            Text("")
         }
-        .padding()
+        .font(.title.bold())
+        .onAppear(){
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { timer in
+            if tapCount > 1 {
+                withAnimation(.bouncy(duration:2)) {
+                    Yoffset -= 54
+                }
+                tapCount -= 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
